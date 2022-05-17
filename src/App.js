eac2c1b5e7ceb23453291d9a41302af93f5cf4b6
Finsternavy <finsternavy@gmail.com>
2022-05-17 10:42:39 -0500

    107_assignment_2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <h1>Welcome to my Store</h1>
+      <h1>At Ease Web Development</h1>
       <Catalog></Catalog>
       <Footer></Footer>
     </div>
--- src/components/product.jsx
+++ src/components/product.jsx
@@ -5,11 +5,11 @@ const Product = () => {
     return (
         <div className='product'>
             <div className="product-img-container">
-                <img src="https://picsum.photos/250/200"  alt="Product Name" className="product-img" />
+                <img src="https://picsum.photos/300/200"  alt="Product Name" className="product-img" />
             </div>
             <div className="product-bottom-container">
                 <div className="product-info">
-                    <h3 className="product-title">Product Title</h3>
+                    <h2 className="product-title">Product Title</h2>
                     <label className="product-price">$Free.99</label>
                     <label className="product-total">Total</label>
                 </div>
